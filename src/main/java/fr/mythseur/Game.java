5e647a5f7a8a5ada292ee1e04652696deb5d2424
mythseur <contact@mythseur.fr>
2021-05-06 17:12:15 +0200

    I'm Rich and I'm Growing

--- src/main/java/fr/mythseur/Game.java
+++ src/main/java/fr/mythseur/Game.java
@@ -23,8 +23,17 @@ public class Game {
     }
 
     Action getNextAction() {
-        Optional<Action> max = possibleActions.stream().filter(action -> action.type == EAction.COMPLETE).max(Comparator.comparingInt(action -> board.get(action.targetCellIdx).richess));
-        return max.orElseGet(() -> possibleActions.get(0));
+        Optional<Action> maxTreeSize3 = possibleActions.stream().filter(action -> action.type == EAction.COMPLETE).max(Comparator.comparingInt(action -> board.get(action.targetCellIdx).richess));
+        if (maxTreeSize3.isPresent())
+            return maxTreeSize3.get();
+
+        Optional<Action> maxTreeSize2 = possibleActions.stream().filter(action -> action.type == EAction.GROW).filter(action -> trees.stream().filter(tree -> tree.cellIndex == action.targetCellIdx).findFirst().get().size == 2).max(Comparator.comparingInt(action -> board.get(action.targetCellIdx).richess));
+        if (maxTreeSize2.isPresent())
+            return maxTreeSize2.get();
+
+        Optional<Action> maxTreeSize1 = possibleActions.stream().filter(action -> action.type == EAction.GROW).filter(action -> trees.stream().filter(tree -> tree.cellIndex == action.targetCellIdx).findFirst().get().size == 1).max(Comparator.comparingInt(action -> board.get(action.targetCellIdx).richess));
+        return maxTreeSize1.orElseGet(() -> possibleActions.get(0));
+
     }
 
 }
